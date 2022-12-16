0a85dabbed7019b93257e821a490569b89e8955c
test <test@test.com>
2022-12-16 15:59:44 +0900

    221215 과제

--- src/main/java/com/test/lesson04/Quiz01.java
+++ src/main/java/com/test/lesson04/Quiz01.java
@@ -1,5 +1,10 @@
 package com.test.lesson04;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,15 +16,24 @@ import com.test.common.MysqlService;
 public class Quiz01 extends HttpServlet {
 	
 	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		response.setContentType("text/plain");
 		// 연결
 		MysqlService ms = MysqlService.getInstance();
 		ms.connect();
 
 		// select 출력
-		PrintWrite out
-		
+		PrintWriter out = response.getWriter();
+		String selectQuery = "select * from `real_estate` order by `id` desc limit 10;";
+		try {
+			ResultSet resultSet = ms.select(selectQuery);
+			while (resultSet.next()) {
+				out.println("매물 주소: " + resultSet.getString("address") 
+				+ ", 면적: " + resultSet.getInt("area") + ", 타입: " + resultSet.getString("type"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	
 		// 해제
 		ms.disconnect();
 	}
